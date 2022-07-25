bfd4b2d1cc3aebbc9cbf7c78a9692076101597b7
John Ericson <946327+JohnEricson@users.noreply.github.com>
2022-07-25 17:50:35 +0200

    BUG: Set executable on install.sh file. It seems that VS Code requres this. MISC: Improved documentation how to configure VS Code.

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Needed by VS Code Remote - Containers extension to be able to launch dotFiles creation.
 # Configure in VS Code like this:
-#   "dotfiles.repository": "JohnEricson/dotFiles"
-# Rest of dotfiles.* should be default.
+#   "dotfiles.repository": "JohnEricson/dotFiles",
+#   "dotfiles.targetPath": "~/.dotfiles",
+#   "dotfiles.installCommand": "~/.dotfiles/install.sh"
 
 sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply JohnEricson
 
