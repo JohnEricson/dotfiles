a3e82c6ec76940820f87119936949753cb56ba81
John Ericson <946327+JohnEricson@users.noreply.github.com>
2023-10-26 14:24:29 +0200

    FEAT: Added bash script to load this users config, aliases and environment variables from another user. Good when other users on same server wants to use your config.

---
+++ Documents/Scripts/use_my_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script to use this users config.
+# Loads the users bash config, aliases and useful
+# environment variables.
+# 
+# Run like this:
+#
+# . ./use_my_config.sh
+
+script_absolute_path=$(realpath "${BASH_SOURCE[0]}")
+script_dir=$(dirname "${script_absolute_path}")
+home_dir=$(echo $script_dir | sed -e 's/\(\/home\/[[:alnum:]]*\)\/.*/\1/g')
+
+# Load config files.
+source "${home_dir}/.bashrc"
+source "${home_dir}/.bash_aliases"
+source "${home_dir}/.environments"
+
+# Override config
+export PATH="${home_dir}/bin:$PATH"
+
