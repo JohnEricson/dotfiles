47c2df4a271efce326052fcad2965f6d72974174
John Ericson <946327+JohnEricson@users.noreply.github.com>
2023-10-26 15:11:01 +0200

    IMPR: Also override environment variable XDG_CONFIG_HOME and XDG_DATA_HOME. These are required for Neovim to work when running from another users config.

--- Documents/Scripts/use_my_config.sh
+++ Documents/Scripts/use_my_config.sh
@@ -16,8 +16,14 @@ source "${home_dir}/.bashrc"
 source "${home_dir}/.bash_aliases"
 source "${home_dir}/.environments"
 
-# Override config
+
+# Override configs.
 export PATH="${home_dir}/bin:$PATH"
+
+# Override freedesktop.org standard dirs. Used by Neovim.
+# Based on: https://neovim.io/doc/user/starting.html#standard-path
+export XDG_CONFIG_HOME="${home_dir}/.config"
+export XDG_DATA_HOME="${home_dir}/.local/share"
 
 # Override Neovim/Vim config.
 export VIMINIT="so ${home_dir}/.vimrc | set runtimepath^=${home_dir}/.vim"
