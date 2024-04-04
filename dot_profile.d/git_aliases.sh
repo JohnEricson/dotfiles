a96de61a6c4d9aeb19bb7e70b1588d14411c2550
John Ericson <946327+JohnEricson@users.noreply.github.com>
2024-04-04 17:30:26 +0000

    BUG: Fix so bash alias gbd only returns local branches.

--- dot_profile.d/git_aliases.sh
+++ dot_profile.d/git_aliases.sh
@@ -11,8 +11,22 @@ function_exists() {
 }
 
 for al in `git --list-cmds=alias`; do
+    # Greate bash aliases named g* for all my git aliases.
     alias g$al="git $al"
 
+    # Setup auto completion for my git aliases.
     complete_func=_git_$(__git_aliased_command $al)
     function_exists $complete_func && __git_complete g$al $complete_func
 done
+
+
+# Make alias gbd only return local branches.
+_gbd_complete() {
+    # Get the current word being completed
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    # Get a list of local branches using `git branch` and remove the '*' character for the current branch
+    local branches=$(git branch | sed 's/^\*//')
+    # Perform completion using the list of branches
+    COMPREPLY=($(compgen -W "$branches" -- "$cur"))
+}
+complete -F _gbd_complete gbd
