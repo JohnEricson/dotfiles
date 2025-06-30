bd2589bddd28f4d609348dc053759f924e92bbc4
John Ericson <946327+JohnEricson@users.noreply.github.com>
2025-06-30 18:00:15 +0200

    BUG: Now the PowerShell profile prompt that uses semantic escape sequences (OSC 133) is compatible with both PowerShell Core and Windows PowerShell.

--- Documents/WindowsPowerShell/profile.ps1
+++ Documents/WindowsPowerShell/profile.ps1
@@ -149,6 +149,9 @@ function Global:__Terminal-Get-LastExitCode {
 # Set the same prompt as on Linux.
 # Based on: https://ss64.com/ps/syntax-prompt.html
 function prompt {
+	# To be compatible with both PowerShell Core and Windows PowerShell.
+	$ESC = [char]27
+
 	# Write home directory as ~ just like Bash.
 	if ("$pwd" -eq "$Env:USERPROFILE") {
 		$pwd_last_dir = '~'
@@ -167,9 +170,9 @@ function prompt {
 	if ($Global:__LastHistoryId -ne -1) {
 		if ($LastHistoryEntry.Id -eq $Global:__LastHistoryId) {
 			# Don't provide a command line or exit code if there was no history entry (eg. ctrl+c, enter on no command)
-			$out += "`e]133;D`a"
+			$out += "$ESC]133;D`a"
 		} else {
-			$out += "`e]133;D;$gle`a"
+			$out += "$ESC]133;D;$gle`a"
 		}
 	}
 
@@ -177,10 +180,10 @@ function prompt {
 	$loc = $($executionContext.SessionState.Path.CurrentLocation);
 
 	# Prompt started
-	$out += "`e]133;A$([char]07)";
+	$out += "$ESC]133;A$([char]07)";
 
 	# CWD
-	$out += "`e]9;9;`"$loc`"$([char]07)";
+	$out += "$ESC]9;9;`"$loc`"$([char]07)";
 
 	# (your prompt here)
 	# $out += "PWSH $loc$('>' * ($nestedPromptLevel + 1)) ";
@@ -192,7 +195,7 @@ function prompt {
 	$out += "$ESC[1m$ESC[37m$Env:USERNAME@$ESC[35m$pwd_last_dir$ESC[37m$ " 
 
 	# Prompt ended, Command started
-	$out += "`e]133;B$([char]07)";
+	$out += "$ESC]133;B$([char]07)";
 
 	$Global:__LastHistoryId = $LastHistoryEntry.Id
 
